008dacf6bff27378e72e6588ec2d17f960edfc7a
nate-hunter <hunter.j.nate@gmail.com>
2023-08-26 22:19:58 -0400

    added comment to describe `linenos`

--- app/snippets/models.py
+++ app/snippets/models.py
@@ -10,7 +10,7 @@ class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
-    linenos = models.BooleanField(default=False)
+    linenos = models.BooleanField(default=False) # If set to true, print the line numbers
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
 
