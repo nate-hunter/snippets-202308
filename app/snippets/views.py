3df5490d0ee036cf384f05f41fc2cbad11e6dbeb
nate-hunter <hunter.j.nate@gmail.com>
2023-08-28 20:52:20 -0400

    added `permission_classes` to `SnippetList` + `SnippetDetail`

--- app/snippets/views.py
+++ app/snippets/views.py
@@ -6,7 +6,7 @@ from rest_framework import generics
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 
 from django.contrib.auth.models import User
 from snippets.serializers import UserSerializer
@@ -15,6 +15,7 @@ from snippets.serializers import UserSerializer
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -23,6 +24,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class UserList(generics.ListAPIView):
