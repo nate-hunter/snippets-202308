a37cd5d49f3c319e0f02dc0ac2de844d837ab548
nate-hunter <hunter.j.nate@gmail.com>
2023-08-28 20:36:25 -0400

    updated views with `UserList` + `UserDetail`

--- app/snippets/views.py
+++ app/snippets/views.py
@@ -8,6 +8,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from django.contrib.auth.models import User
+from snippets.serializers import UserSerializer
+
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
@@ -17,3 +20,13 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
