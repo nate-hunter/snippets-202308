ba9151147a39546f68e9ea70287306f06d4b4a54
nate-hunter <hunter.j.nate@gmail.com>
2023-08-30 10:33:35 -0400

    updated `urlpatterns` in urls with concrete views

--- app/snippets/urls.py
+++ app/snippets/urls.py
@@ -26,20 +26,10 @@ user_detail = UserViewSet.as_view({
 })
 
 urlpatterns = format_suffix_patterns([
-    path('', views.api_root),
-    path('snippets/',
-        views.SnippetList.as_view(),
-        name='snippet-list'),
-    path('snippets/<int:pk>/',
-        views.SnippetDetail.as_view(),
-        name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/',
-        views.SnippetHighlight.as_view(),
-        name='snippet-highlight'),
-    path('users/',
-        views.UserList.as_view(),
-        name='user-list'),
-    path('users/<int:pk>/',
-        views.UserDetail.as_view(),
-        name='user-detail')
+    path('', api_root),
+    path('snippets/', snippet_list, name='snippet-list'),
+    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail')
 ])
